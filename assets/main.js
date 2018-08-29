24ee81e31d38228043693cc8c41e44f7dae6e666
Kevin <danggkev@gmail.com>
2018-08-29 11:57:33 -0700

    Testing JSON API

---
+++ assets/main.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+function test(){
+    //var url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/103b07ef04d782146176e5e9f5408e46/"+ latitude + "," +longitude;
+    var url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=GADBOCMLVZQZV9IS"
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.onload = function(){
+      if(request.status >= 200 && request.status < 400){
+      var data = JSON.parse(this.responseText);
+      console.log("Success");
+      console.log(data['Meta Data']);
+     }
+    else{
+      console.log("Error");
+    };
+  };
+    request.onerror = function(){
+      console.log("Error");
+    };
+    request.send();
+}
+
