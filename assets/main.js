28b95ea6ceeac581303010a63ce3627ab3a997b2
Kevin Dang <kevin@kevins-mac-mini.edwardstech.net>
2018-08-30 20:05:35 -0700

    Working on the API

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,18 @@
 
 
 
+
 function test(){
+    var x = document.getElementById("ticker").value;
     //var url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/103b07ef04d782146176e5e9f5408e46/"+ latitude + "," +longitude;
-    var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=SQ&interval=5min&apikey=GADBOCMLVZQZV9IS"
+    var url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=+x+&interval=5min&apikey=GADBOCMLVZQZV9IS"
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = function(){
       if(request.status >= 200 && request.status < 400){
       var data = JSON.parse(this.responseText);
       console.log("Success");
-      console.log(data['Meta Data']['2. Symbol']);
-      console.log(data['Time Series (Daily)']['2018-08-29']['1. open']);
-      console.log(data['Time Series (Daily)']['2018-08-29']['2. high']);
+      console.log(data);
      }
     else{
       console.log("Error");
@@ -23,4 +23,5 @@ function test(){
     };
     request.send();
 }
+
 
